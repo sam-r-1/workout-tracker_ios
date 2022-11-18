2976c4f4fda3fd82665c9700b7dd63383efe8359
Sam Rankin <sam.rankin.aos@gmail.com>
2022-11-17 21:34:36 -0500

    can change exercise order within a template; UI still WIP

--- workout_tracker/Core/Templates/ModifyTemplate/Views/ModifyTemplateView.swift
+++ workout_tracker/Core/Templates/ModifyTemplate/Views/ModifyTemplateView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ModifyTemplateView: View {
     let modifyMode: ModifyObjectMode
+    @State private var editMode = EditMode.active
     @State private var showDeleteDialog = false
     @State private var showAddExercise = false
     @Environment(\.presentationMode) var presentationMode
@@ -30,58 +31,62 @@ struct ModifyTemplateView: View {
     }
     
     var body: some View {
-        Form() {
-            Section(header: Text("NAME (Required)")) {
-                TextField("Template name", text: $viewModel.name)
-            }
-
-            Section(header: Text("Exercises (add at least one)")) {
-                ForEach(viewModel.exerciseList, id: \.self) { item in
-                    HStack {
-                        Text(item.name)
-                        Spacer()
-                        Button(role: .destructive) {
-                            viewModel.exerciseList.remove(at: viewModel.exerciseList.firstIndex(where: { $0.id == item.id })!)
-                        } label: {
-                            Image(systemName: "delete.left.fill")
-                        }
-
-                    }
-                }
-            }
+        VStack(alignment: .leading) {
+            Text("NAME (Required)")
+            TextField("Template name", text: $viewModel.name)
             
-            Button {
-                showAddExercise.toggle()
-            } label: {
-                Text("Add Exercise")
-                    .bold()
-            }
-            .foregroundColor(Color(.systemGreen))
-            .frame(maxWidth: .infinity, alignment: .center)
+            Spacer()
+            
+            HStack {
+                Text("Exercises")
+                
+                Spacer()
+                
+                Button {
+                    showAddExercise.toggle()
+                } label: {
+                    Text("+ Add")
+                        .bold()
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 16)
+                }
+                .foregroundColor(.white)
+                .background(Color(.systemGreen))
+                .clipShape(Capsule())
 
+            }
+            .padding(.horizontal)
+            
+            List {
+                ForEach(viewModel.exerciseList, id: \.self) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: viewModel.removeExercise)
+                .onMove(perform: viewModel.moveExercise)
+            }
+            .listStyle(.plain)
+            .environment(\.editMode, $editMode)
             
             if modifyMode == .edit {
-                Section {
-                    Button("Delete Template", role: .destructive) {
-                        showDeleteDialog = true
-                    }
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .alert("Delete this template?", isPresented: $showDeleteDialog) {
-                        Button("Delete", role: .destructive) {
-                            viewModel.deleteTemplate()
-                            presentationMode.wrappedValue.dismiss()
-                        }
+                Button("Delete Template", role: .destructive) {
+                    showDeleteDialog = true
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .alert("Delete this template?", isPresented: $showDeleteDialog) {
+                    Button("Delete", role: .destructive) {
+                        viewModel.deleteTemplate()
+                        presentationMode.wrappedValue.dismiss()
                     }
                 }
             }
         }
-        .navigationTitle(viewModel.name.isEmpty ? "Add Template" : viewModel.name)
+        .navigationTitle(viewModel.name.isEmpty ? "New Template" : viewModel.name)
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden(true)
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 Button {
-                    presentationMode.wrappedValue.dismiss()
+                    presentationMode.wrappedValue.dismiss() 
                 } label: {
                     Text("Cancel")
                         .padding(.leading, 12)
@@ -93,7 +98,7 @@ struct ModifyTemplateView: View {
                 Button {
                     viewModel.modifyTemplate()
                 } label: {
-                    Text(modifyMode.submitText)
+                    Text("Save")
                         .padding(.trailing, 12)
                 }
                 .disabled(!formValidated)
@@ -104,7 +109,7 @@ struct ModifyTemplateView: View {
                 viewModel.addExercise(exerciseId)
             }
         }
-        // dismiss view if exercise created successfully
+        // dismiss view if template created successfully
         .onReceive(viewModel.$didCreateTemplate) { success in
             if success {
                 presentationMode.wrappedValue.dismiss()
@@ -115,6 +120,8 @@ struct ModifyTemplateView: View {
 
 struct ModifyTemplateView_Previews: PreviewProvider {
     static var previews: some View {
-        ModifyTemplateView()
+        NavigationView {
+            ModifyTemplateView()
+        }
     }
 }
